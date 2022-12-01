f030067c42cafc443566335b745e60aa439a1e0c
Yael <yaelsc.edu@gmail.com>
2022-12-01 08:48:21 +0200

    added sky and scene assets

--- constants.py
+++ constants.py
@@ -51,9 +51,14 @@ PHONE_LEFT = WIDTH - WIDTH/4.7
 LIGHTS_BTN_CENTER = WIDTH-PHONE_CENTER
 
 # Asset Definitions
-SKY = scale_image(pygame.image.load(PATH + "Backgrounds/background0.png"),1.3)
+SKY_DAY = scale_image(pygame.image.load(PATH + "Backgrounds/day_sky.jpg"),0.25)
+SKY_NIGHT = scale_image(pygame.image.load(PATH + "Backgrounds/night_sky.jpg"),0.5)
+SKY_RAINY = scale_image(pygame.image.load(PATH + "Backgrounds/rainy_sky.jpg"),0.3)
+SKY_SUNNY = scale_image(pygame.image.load(PATH + "Backgrounds/sunny_sky.jpg"),0.25)
+
 SCENE = scale_image(pygame.image.load(PATH + "Scenes\scene_1.png"),1.4)
-LEVEL_IMGS = [(SKY, (0,0)), (SCENE, (0, SCENE_HEIGHT_START))]
+SCENE_DARK = scale_image(pygame.image.load(PATH + "Scenes\scene_1_dark.png"),1.4)
+LEVEL_IMGS = [(SKY_DAY, (0,0)), (SCENE, (0, SCENE_HEIGHT_START))]
 
 DASHBOARD_RECT_HOR = pygame.Rect(0, DASHBOARD_HOR_TOP, WIDTH,HEIGHT/4)
 DASHBOARD_RECT_VER = pygame.Rect(WIDTH - WIDTH/4.5, 0, WIDTH/4, HEIGHT) 
@@ -63,7 +68,7 @@ PHONE = scale_image(pygame.image.load(PATH + "Dashboard/phone.png"),0.55)
 
 # Position Definitions - center
 SPEEDOMETER_POS = (MIRROR_CENTER-SPEEDOMETER.get_rect().centerx, DASHBOARD_HOR_TOP-SPEEDOMETER.get_rect().centery/2)
-MIRROR_POS = (MIRROR_CENTER-MIRROR.get_rect().centerx, MIRROR.get_rect().centery/5)
+MIRROR_POS = (MIRROR_CENTER-MIRROR.get_rect().centerx, MIRROR.get_rect().centery/3)
 PHONE_POS = (PHONE_CENTER-PHONE.get_rect().centerx,HEIGHT-DASHBOARD_HOR_TOP-PHONE.get_rect().centery/4)
 DASH_IMGS = [(SPEEDOMETER, SPEEDOMETER_POS), (MIRROR, MIRROR_POS),(PHONE, PHONE_POS)]
 
@@ -82,14 +87,12 @@ LIGHTS_BTN_POS = (MIRROR_CENTER/2-BTN_IMGS_OFF[0].get_rect().centerx/2, DASHBOAR
 
 LEFT_BLINK_POS = (SPEEDOMETER_POS[0]-BTN_IMGS_OFF[1].get_rect().centerx ,DASHBOARD_HOR_TOP-10)
 RIGHT_BLINK_POS = ((SPEEDOMETER_POS[0]+SPEEDOMETER_RIGHT)/2+BTN_IMGS_OFF[2].get_rect().centerx ,DASHBOARD_HOR_TOP-10)
-#RIGHT_BLINK_POS = ((SPEEDOMETER_RIGHT+PHONE_CENTER)/3+BTN_IMGS_OFF[2].get_rect().centerx, DASHBOARD_HOR_TOP-10)
 
 WIPERS_BTN_POS = (RIGHT_BLINK_POS[0]+BTN_IMGS_OFF[3].get_rect().centerx*1.5, DASHBOARD_HOR_TOP)
 AC_BTN_POS = (WIPERS_BTN_POS[0]+BTN_IMGS_OFF[4].get_rect().centerx, DASHBOARD_HOR_TOP)
-#AC_BTN_POS = (PHONE_LEFT-BTN_IMGS_OFF[4].get_rect().centerx/2, DASHBOARD_HOR_TOP)
-#MUSIC_BTN_POS = (AC_BTN_POS[0]+BTN_IMGS_OFF[5].get_rect().centerx, DASHBOARD_HOR_TOP)
 MUSIC_BTN_POS = (PHONE_CENTER-BTN_IMGS_OFF[5].get_rect().centerx/2, DASHBOARD_HOR_TOP)
 
+# Buttons Blueprints for DashboardButton
 MENU_BTN_BLP = [MENU_BTN_IMG,MENU_BTN_IMG,BTN_SCALE,*MENU_BTN_POS]
 LIGHTS_BTN_BLP = [BTN_IMGS_OFF[0],BTN_IMGS_ON[0],BTN_SCALE,*LIGHTS_BTN_POS]
 LEFT_BLINK_BLP = [BTN_IMGS_OFF[1],BTN_IMGS_ON[1],BLINKER_SCALE,*LEFT_BLINK_POS]
@@ -100,6 +103,7 @@ MUSIC_BTN_BLP = [BTN_IMGS_OFF[5],BTN_IMGS_ON[5],BTN_SCALE,*MUSIC_BTN_POS]
 
 SPEEDOMETER_TEXT_POS = (MIRROR_CENTER,SPEEDOMETER_POS[1]+SPEEDOMETER.get_rect().centery)
 
+# Road Users Definitions 
 RED_CAR = scale_image(pygame.image.load(PATH + "Cars/red_car.png"), 0.3)
 
 
