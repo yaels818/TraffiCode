fd9acb9a5459a91dce8c86c2757f42c8c99de943
Yael <yaelsc.edu@gmail.com>
2022-12-02 06:18:23 +0200

    removed direct Sprite inheritance for PlayerSprite

--- RoadUsers/PlayerSprite.py
+++ RoadUsers/PlayerSprite.py
@@ -3,17 +3,17 @@ import pygame
 from RoadUsers import RoadUser
 import constants
 
-class PlayerSprite(RoadUser, pygame.sprite.Sprite):
+class PlayerSprite(RoadUser):
 
     IMG = constants.RED_CAR
     START_POS = (150,150)
 
     def __init__(self,max_vel, rotation_vel):
-        # Call the parent class's constructor
-        pygame.sprite.Sprite.__init__(self)
+        
+        
         RoadUser.__init__(self,max_vel, rotation_vel)
         self.image = constants.RED_CAR
-        self.rect = self.image.get_rect(center = self.START_POS)
+        self.rect = self.image.get_rect(topleft = self.START_POS)
 
     def reduce_speed(self):
         # Reduce the velocity by half the acceleration, if negative then just stop moving 
@@ -27,4 +27,9 @@ class PlayerSprite(RoadUser, pygame.sprite.Sprite):
 
     def reset(self):
          RoadUser().reset()
+
+    def draw(self, win):
+        RoadUser.draw(self,win)
+        pygame.draw.rect(win,constants.BLUE,self.rect,2)
+
 
