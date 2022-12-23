a4698a3b3ddfb9564787c18831c4cde6437eab56
Yael <yaelsc.edu@gmail.com>
2022-12-23 16:35:01 +0200

    draw function is now inherited from RoadUser

--- RoadUsers/PlayerSprite.py
+++ RoadUsers/PlayerSprite.py
@@ -1,18 +1,18 @@
 import pygame
 
 from RoadUsers import RoadUser
-import constants
+from constants import RED_CAR
 
 class PlayerSprite(RoadUser):
 
-    IMG = constants.RED_CAR
+    IMG = RED_CAR
 
     def __init__(self,start_pos):
         RoadUser.__init__(self,start_pos)
 
     def reduce_speed(self):
         # Reduce the velocity by half the acceleration, if negative then just stop moving 
-        self.vel = max(self.vel - 2 * self.acceleration, 0)
+        self.vel = max(self.vel - self.acceleration/2, 0)
         self.move()
 
     def bounce(self):
@@ -22,9 +22,3 @@ class PlayerSprite(RoadUser):
 
     def reset(self):
          RoadUser().reset()
-
-    def draw(self, win):
-        RoadUser.draw(self,win)
-        pygame.draw.rect(win,constants.BLUE,self.rect,2)
-
-
