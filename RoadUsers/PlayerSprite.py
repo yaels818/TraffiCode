995f389204671fd93f41447579af02a970f4d14e
Yael <yaelsc.edu@gmail.com>
2022-12-24 11:52:45 +0200

    added check_collision_with_mask

--- RoadUsers/PlayerSprite.py
+++ RoadUsers/PlayerSprite.py
@@ -24,5 +24,15 @@ class PlayerSprite(RoadUser):
         self.vel = -self.vel/2
         self.move()
 
+    def check_collision_with_mask(self, mask, x = 0, y = 0):
+        car_mask = pygame.mask.from_surface(self.image)
+
+        # Calculate displacement between the 2 masks
+        offset = (int(self.x - x), int(self.y - y))
+
+        # Point of intersection - if there was poi, the objects did collide
+        poi = mask.overlap(car_mask, offset) 
+        return poi
+
     def reset(self):
          RoadUser().reset()
