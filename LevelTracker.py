a79dad349c9ffff10c346e6cf153e9426cbda721
Yael <yaelsc.edu@gmail.com>
2023-03-05 14:16:27 +0200

    added driving_against_traffic

--- LevelTracker.py
+++ LevelTracker.py
@@ -9,7 +9,7 @@ class LevelTracker():
         self.sidewalk_hits = 0
         self.parking_lot_hits = 0
         self.roundabout_hits = 0
-        
+        self.against_traffic = 0
 
     def increase_level(self):
         self.level += 1
@@ -23,12 +23,16 @@ class LevelTracker():
     def add_sidewalk_hit(self):
         self.sidewalk_hits += 1
 
+    def add_driving_against_traffic(self):
+        self.against_traffic += 1
+
     def reset(self):
         self.level = 1
 
         self.parking_lot_hits = 0
         self.roundabout_hits = 0
         self.sidewalk_hits = 0
+        self.against_traffic = 0
     
     def display(self):
         """
@@ -43,7 +47,8 @@ class LevelTracker():
         sidewalk_hits_text = CLIP_FONT.render(f"Sidewalk hits: {self.sidewalk_hits}", 1, RED)
         pl_hits_text = CLIP_FONT.render(f"Parking lot hits: {self.parking_lot_hits}", 1, RED)
         rbt_hits_text = CLIP_FONT.render(f"Roundabout hits: {self.roundabout_hits}", 1, RED)
-        
+        against_traffic_text = CLIP_FONT.render(f"Wrong direction: {self.against_traffic}", 1, RED)
+
         # define position for each text, center by text rect center
         timer_text_pos = (MIRROR_CENTER-timer_text.get_rect().centerx,MIRROR_POS[1]+timer_text.get_rect().centery)
         level_text_pos = (CLIP_LEFT+0.5*level_text.get_rect().centerx,CLIP_TOP+level_text.get_rect().centery)
@@ -51,7 +56,7 @@ class LevelTracker():
         texts_to_display = [(level_text, level_text_pos), 
                             (timer_text, timer_text_pos)]
 
-        hit_texts = [sidewalk_hits_text,pl_hits_text,rbt_hits_text]
+        hit_texts = [sidewalk_hits_text,pl_hits_text,rbt_hits_text,against_traffic_text]
         line_space = -3
         for t in hit_texts:
             pos = (CLIP_CENTER-t.get_rect().centerx, RBT_RIGHT_CENTER[1]+line_space*t.get_rect().centery)
