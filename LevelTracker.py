b18d3f0c9fc231841773b3a1acc64cec5ad9e95e
Yael <yaelsc.edu@gmail.com>
2023-04-08 09:42:44 +0300

    replaced inaccurate parking with accurate

--- LevelTracker.py
+++ LevelTracker.py
@@ -1,13 +1,13 @@
 """
-Author: Yael Sch
+Author: @yaels818
 Description: LevelTracker module, contains definitions needed to track the game status
-            (current level, time for each level, violations).
+            (current level, time for each level, violations, good behavior).
 Notes: 
     
 """
 # Imports
 import time
-from constants import WIN, CLIP_FONT, DASH_FONT, BLACK, RED, ORANGE, GREEN, CLIP_LEFT, CLIP_TOP, CLIP_CENTER, MIRROR_CENTER,MIRROR_POS, RBT_RIGHT_CENTER
+from constants import WIN, CLIP_FONT, DASH_FONT, BLACK, RED, GREEN, CLIP_LEFT, CLIP_TOP, CLIP_CENTER, MIRROR_CENTER,MIRROR_POS, RBT_RIGHT_CENTER
 #-------------------------------------------------------------------------
 class LevelTracker():
     
@@ -40,7 +40,7 @@ class LevelTracker():
 
         self.roundabout_hits = 0
         self.parking_lot_hits = 0
-        self.parking_inaccurate = 0
+        self.accurate_parking = 0
 
     def increase_level(self):
         self.level += 1
@@ -79,8 +79,8 @@ class LevelTracker():
     def add_parking_lot_hit(self):
         self.parking_lot_hits += 1
     
-    def add_parking_inaccurate(self):
-        self.parking_inaccurate += 1
+    def add_accurate_parking(self):
+        self.accurate_parking += 1
 
     def reset(self):
         self.level = 1
@@ -93,7 +93,7 @@ class LevelTracker():
 
         self.roundabout_hits = 0
         self.parking_lot_hits = 0
-        self.parking_inaccurate = 0
+        self.accurate_parking = 0
     
     def display(self):
         """
@@ -111,7 +111,8 @@ class LevelTracker():
 
         rbt_hits_text = CLIP_FONT.render(f"Roundabout hits: {self.roundabout_hits}", 1, RED)
         pl_hits_text = CLIP_FONT.render(f"Parking lot hits: {self.parking_lot_hits}", 1, RED)
-        parking_inaccurate_text = CLIP_FONT.render(f"Inaccurate parking: {self.parking_inaccurate}", 1, ORANGE)
+        
+        accurate_parking_text = CLIP_FONT.render(f"Accurate parking angle: {self.accurate_parking}", 1, GREEN)
 
 
         # define position for each text, center by text rect center
@@ -122,14 +123,22 @@ class LevelTracker():
                             (timer_text, timer_text_pos)]
 
         hit_texts = [sidewalk_hits_text, against_traffic_text, over_solid_lane_text,
-                    rbt_hits_text, pl_hits_text, parking_inaccurate_text]
-                    
+                    rbt_hits_text, pl_hits_text]
+
+        bonus_texts = [accurate_parking_text]
+
         line_space = -3
         for t in hit_texts:
             pos = (CLIP_CENTER-t.get_rect().centerx, RBT_RIGHT_CENTER[1]+line_space*t.get_rect().centery)
             line_space += 2.5
             texts_to_display.append((t,pos))
         
+        line_space += 3
+        for t in bonus_texts:
+            pos = (CLIP_CENTER-t.get_rect().centerx, RBT_RIGHT_CENTER[1]+line_space*t.get_rect().centery)
+            line_space += 2.5
+            texts_to_display.append((t,pos))
+
         for txt, pos in texts_to_display:
             # Draw this img in this position
             WIN.blit(txt, pos)  
