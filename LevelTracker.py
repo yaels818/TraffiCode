d6e63a3bdb3ae91f047032fcd351eded92a08073
Yael <yaelsc.edu@gmail.com>
2023-04-14 19:56:43 +0300

    added peds and cars hits

--- LevelTracker.py
+++ LevelTracker.py
@@ -7,7 +7,7 @@ Notes:
 """
 # Imports
 import time
-from constants import WIN, CLIP_FONT, DASH_FONT, BLACK, RED, GREEN, CLIP_LEFT, CLIP_TOP, CLIP_CENTER, MIRROR_CENTER,MIRROR_POS, RBT_RIGHT_CENTER
+from constants import WIN, CLIP_FONT, DASH_FONT, BLACK, RED, ORANGE, GREEN, CLIP_LEFT, CLIP_TOP, CLIP_CENTER, MIRROR_CENTER,MIRROR_POS, RBT_RIGHT_CENTER
 #-------------------------------------------------------------------------
 class LevelTracker():
     
@@ -34,12 +34,16 @@ class LevelTracker():
 
         self.time_to_add_sprites = 0 
 
+        self.peds_hits = 0
+        self.cars_hits = 0
+
         self.sidewalk_hits = 0
         self.over_solid_lane = 0
         self.against_traffic = 0
 
         self.roundabout_hits = 0
         self.parking_lot_hits = 0
+        
         self.accurate_parking = 0
 
     def increase_level(self):
@@ -64,6 +68,12 @@ class LevelTracker():
     def increase_time_to_add_sprites(self):
         self.time_to_add_sprites += 1
 
+    def add_ped_hit(self):
+        self.peds_hits += 1
+
+    def add_car_hit(self):
+        self.cars_hits += 1
+
     def add_sidewalk_hit(self):
         self.sidewalk_hits += 1
 
@@ -87,6 +97,9 @@ class LevelTracker():
         self.level_started = False
         self.level_start_time = 0
 
+        self.peds_hits = 0
+        self.cars_hits = 0
+
         self.sidewalk_hits = 0
         self.over_solid_lane = 0
         self.against_traffic = 0
@@ -105,29 +118,40 @@ class LevelTracker():
         timer_text = DASH_FONT.render(f"Time: {self.get_level_time()} sec", 1, BLACK)
         level_text = CLIP_FONT.render(f"Level {self.level}", 1, BLACK)
 
-        sidewalk_hits_text = CLIP_FONT.render(f"Sidewalk hits: {self.sidewalk_hits}", 1, RED)
-        against_traffic_text = CLIP_FONT.render(f"Wrong direction: {self.against_traffic}", 1, RED)
-        over_solid_lane_text = CLIP_FONT.render(f"Over solid lanes: {self.over_solid_lane}", 1, RED)
+        peds_hits_text = CLIP_FONT.render(f"Pedestrians hits: {self.peds_hits}", 1, RED)
+        cars_hits_text = CLIP_FONT.render(f"Other cars hits: {self.cars_hits}", 1, RED)
 
-        rbt_hits_text = CLIP_FONT.render(f"Roundabout hits: {self.roundabout_hits}", 1, RED)
-        pl_hits_text = CLIP_FONT.render(f"Parking lot hits: {self.parking_lot_hits}", 1, RED)
+        sidewalk_hits_text = CLIP_FONT.render(f"Sidewalk hits: {self.sidewalk_hits}", 1, ORANGE)
+        over_solid_lane_text = CLIP_FONT.render(f"Over solid lanes: {self.over_solid_lane}", 1, ORANGE)
+        against_traffic_text = CLIP_FONT.render(f"Wrong direction: {self.against_traffic}", 1, ORANGE)
+
+        rbt_hits_text = CLIP_FONT.render(f"Roundabout hits: {self.roundabout_hits}", 1, ORANGE)
+        pl_hits_text = CLIP_FONT.render(f"Parking lot hits: {self.parking_lot_hits}", 1, ORANGE)
         
-        accurate_parking_text = CLIP_FONT.render(f"Accurate parking angle: {self.accurate_parking}", 1, GREEN)
-
+        accurate_parking_text = CLIP_FONT.render(f"Accurate parking: {self.accurate_parking}", 1, GREEN)
 
         # define position for each text, center by text rect center
         timer_text_pos = (MIRROR_CENTER-timer_text.get_rect().centerx,MIRROR_POS[1]+timer_text.get_rect().centery)
-        level_text_pos = (CLIP_LEFT+0.5*level_text.get_rect().centerx,CLIP_TOP+level_text.get_rect().centery)
+        level_text_pos = (CLIP_LEFT+0.5*level_text.get_rect().centerx,CLIP_TOP+level_text.get_rect().centery/8)
         
         texts_to_display = [(level_text, level_text_pos), 
                             (timer_text, timer_text_pos)]
 
-        hit_texts = [sidewalk_hits_text, against_traffic_text, over_solid_lane_text,
+        accident_texts = [peds_hits_text, cars_hits_text]
+        
+        hit_texts = [sidewalk_hits_text, over_solid_lane_text, 
+                    against_traffic_text,
                     rbt_hits_text, pl_hits_text]
 
         bonus_texts = [accurate_parking_text]
 
-        line_space = -3
+        line_space = -4
+        for t in accident_texts:
+            pos = (CLIP_CENTER-t.get_rect().centerx, RBT_RIGHT_CENTER[1]+line_space*t.get_rect().centery)
+            line_space += 2.5
+            texts_to_display.append((t,pos))
+        
+        line_space += 3
         for t in hit_texts:
             pos = (CLIP_CENTER-t.get_rect().centerx, RBT_RIGHT_CENTER[1]+line_space*t.get_rect().centery)
             line_space += 2.5
