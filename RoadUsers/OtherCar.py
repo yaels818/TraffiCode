73ced8dea5cc0f4f931a85a487b67c25a9abb3a5
Yael <yaelsc.edu@gmail.com>
2023-04-23 21:23:09 +0300

    removed unnecessary functions and imports

--- RoadUsers/OtherCar.py
+++ RoadUsers/OtherCar.py
@@ -4,10 +4,13 @@ Description: OtherCar module, contains other car sprites
 (the player must avoid crashing into them)
 """
 
-import pygame, math, random
+import math, random
 
 from RoadUsers import RoadUser
-from constants import WEST, YELLOW_CAR, WHITE_TRUCK, BLUE_VAN ,CAR_PATH_YAAR_TILL_ESHEL, CAR_PATH_ESHEL_TILL_ROTEM, CAR_PATH_ELLA_TILL_EREZ, CAR_PATH_ROTEM_TILL_ELLA
+from constants import DIRECTION_SMOOTH, EAST, SOUTH, YELLOW_CAR, WHITE_TRUCK, BLUE_VAN ,\
+    CAR_PATH_YAAR_TILL_ESHEL, CAR_PATH_ESHEL_TILL_ROTEM, \
+    CAR_PATH_ELLA_TILL_EREZ, CAR_PATH_ROTEM_TILL_ELLA, WIN
+from utils import blit_rotate_center
 
 class OtherCar(RoadUser):
 
@@ -51,89 +54,11 @@ class OtherCar(RoadUser):
         RoadUser.__init__(self, IMG, self.path[0])
 
         self.current_point = 0
-        self.vel = self.max_vel
-        
-    def reduce_speed(self):
-        # Reduce the velocity by half the acceleration, if negative then just stop moving 
-        self.vel = max(self.vel - self.acceleration/2, 0)
-        self.move()
+        self.vel = 1
 
-    def draw_points(self, win):
-        for point in self.path:
-            # Draw a red point of radius 5 in the path
-            pygame.draw.circle(win, (255,0,0), point, 5)
-        
-    def calculate_angle(self):
-        # Get coordinates for target point
-        target_x, target_y = self.path[self.current_point]
-
-        # Calculate displacement between the sprite and the target point
-        x_diff = target_x - self.x
-        y_diff = target_y - self.y
-
-        if y_diff == 0:
-            # If there is no y difference 
-            # => the car is horizontal to the point 
-            # (so either 90 or 270 degrees)
-            desired_radian_angle = math.pi / 2
-        else:
-            # The angle between the car and the target point
-            # (will always return an acute angle = less than 90 degrees)
-            desired_radian_angle = math.atan(x_diff / y_diff) # arctan()
-
-        # If the target is downwards from the sprite
-        # => the needed turn to get to the target point 
-        # has to be more extreme than the calculated acute angle
-        if target_y > self.y:
-            # Correct the angle to make sure the car will be heading in the correct direction
-            desired_radian_angle += math.pi
-
-        difference_in_angle = self.angle - math.degrees(desired_radian_angle)
-
-        # If the difference is drastic 
-        # => there is a more efficient angle to get to the target point
-        if difference_in_angle >= 180:
-            difference_in_angle -= 360
-            
-        # Make sure the car doesn't over/undershoot the angle 
-        # (avoid stuttering and over-corrections)
-        if difference_in_angle > 0:
-            # If the diff is less than rotation_vel 
-            # => we will snap immediately to the diff angle and stay on it
-            self.angle -= max(self.rotation_vel, difference_in_angle)
-        else:
-            self.angle += max(self.rotation_vel, abs(difference_in_angle)) 
-            
-        #print(self.angle)   
-        
-    def update_path_point(self):
-            # Get the next target point from the pre-made path
-            target = self.path[self.current_point]
-
-            # Create a rectangle with the car as top-left corner 
-            # (because we have the img but the img doesn't know where it is)
-            rect = pygame.Rect(self.x, self.y, self.image.get_width(), self.image.get_height())
-
-            # If we collided with a target point, move on to the next one
-            if rect.collidepoint(*target):
-                self.current_point += 1
-
-    def move(self):
-        # If there is no point to move to
-        if self.current_point >= len(self.path):
-            #self.kill()
-            return
-
-        # Calculate and shift the car to the needed angle for the next point
-        self.calculate_angle()
-
-        # See if we need to move to the next point
-        self.update_path_point()
-        super().move()
-           
     def next_level(self, level):
         
         # Increase computer's vel 0.2 each level - will never go faster than the player's
         self.vel = self.max_vel + (level + 1) * 0.2
-
         self.current_point = 0
+        
